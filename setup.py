4a276e6361435072d85eafd8155065eeae98f0ed
Vikash Kothary <kothary.vikash@gmail.com>
2019-05-05 19:52:33 +0100

    Create pypi setup using pbr

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" setup.py - PyPi package for Ocean Emails."""
+
+from setuptools import setup
+
+setup(
+    setup_requires=['pbr'],
+    pbr=True
+)
